93d68559c659fea2f6c6229132a4899ce4c21a2a
n18011 <n18011@std.it-college.ac.jp>
2018-10-22 13:26:52 +0900

    Return to the origin

--- 1019/iterator/Main.java
+++ 1019/iterator/Main.java
@@ -1,25 +1,15 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String [] args) {
-        // BookShelf bookShelf = new BookShelf(4);
-        List<String> bookShelf = new ArrayList<>();
-        //bookShelf.appendBook(new Book("80日間世界一周"));
-        bookShelf.add("80日間世界一周");
-        //bookShelf.appendBook(new Book("聖書"));
-        bookShelf.add("聖書");
-        //bookShelf.appendBook(new Book("シンデレラ"));
-        bookShelf.add("シンデレラ");
-        //bookShelf.appendBook(new Book("あしながおじさん"));
-        bookShelf.add("あしながおじさん");
-        //bookShelf.appendBook(new Book("あしながおじさん"));
-        bookShelf.add("あしながおじさん");
-        //Iterator it = bookShelf.iterator();
-        //while(it.hasNext()) {
-            //Book book = (Book)it.next();
-            //System.out.println(book.getName());
-        //}
-        System.out.println(bookShelf);
+        BookShelf bookShelf = new BookShelf(4);
+        bookShelf.appendBook(new Book("80日間世界一周"));
+        bookShelf.appendBook(new Book("聖書"));
+        bookShelf.appendBook(new Book("シンデレラ"));
+        bookShelf.appendBook(new Book("あしながおじさん"));
+        bookShelf.appendBook(new Book("あしながおじさん"));
+        Iterator it = bookShelf.iterator();
+        while(it.hasNext()) {
+            Book book = (Book)it.next();
+            System.out.println(book.getName());
+        }
     }
 }
